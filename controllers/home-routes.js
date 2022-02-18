d2404245c5c7d88e0a0e1423489ec2f3d7555a9b
l bates childs <labchilds@gmail.com>
2022-02-18 00:16:37 -0500

    add single post view and route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,4 +37,44 @@ router.get('/', (req, res) => {
 
 });
 
+// view a single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_title',
+            'post_body',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at', 'post_id', 'user_id'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPost => {
+       if (!dbPost) {
+           res.json({ message: `No post with id ${req.params.id}`});
+           return;
+       }
+       // serialize post data
+       const post = dbPost.get({ plain: true });
+       res.render('single-post', { post }); 
+    })
+    
+})
+
 module.exports = router;
