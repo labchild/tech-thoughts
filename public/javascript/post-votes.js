6988fb1b2e45fb37a6faecab7f85d02e3e5efc8d
l bates childs <labchilds@gmail.com>
2022-02-19 14:53:35 -0500

    fix double clikc on show votes button

--- public/javascript/post-votes.js
+++ public/javascript/post-votes.js
@@ -1,15 +1,14 @@
 async function showVotesHandler(e) {
-    // const id = e.target.getAttribute('id');
-    // const voteEl = e.target.sibling;
+    e.preventDefault();
     // select grandparent as post meta
     const postMetaEl = e.target.parentElement.parentElement;
     // select second child as vote el
     const voteEl = postMetaEl.querySelector('.post-votes');
 
-    if (voteEl.style.display === 'none') {
-        voteEl.style.display = 'block';
-    } else {
+    if (voteEl.style.display === 'block') {
         voteEl.style.display = 'none';
+    } else {
+        voteEl.style.display = 'block';
     }
 };
 
