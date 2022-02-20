1c498fa2399388b493a32011decf38bcf95c6b4e
l bates childs <labchilds@gmail.com>
2022-02-20 15:10:39 -0500

    add edit post handler

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,5 +1,34 @@
 async function editPostFormHandler(e) {
     e.preventDefault();
+    const formEl = e.target;
+    
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const post_title = formEl.querySelector('#edit-title').value.trim();
+    const post_body = formEl.querySelector('#edit-body').value.trim();
+    
+    if (post_title && post_body) {
+        // send updated post to database, await response
+        const response = await fetch(`/api/posts/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                post_title,
+                post_body
+            }),
+            headers: {
+                'Content-Type':'application/json'
+            }
+        });
+        console.log(response);
+        // send back to dashboard or alert to error
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(`${response.statusText}
+            Something went wrong...`);
+        }
+    }
 };
 
 async function deletePostHandler(e) {
