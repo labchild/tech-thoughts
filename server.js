d16841a997936308459913e8bae728eabb1bca5b
l bates childs <labchilds@gmail.com>
2022-02-21 17:53:21 -0500

    fixing deployed db

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(session(sess));
 app.use(routes);
 
 // initiate connection to DB, start server
-sequelize.sync({ force: true })
+sequelize.sync({ force: false })
     .then(() => {
         app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     });
