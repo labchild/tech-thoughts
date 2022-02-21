ff46ef07f3cb0c302b6c78528df924e2556a07ed
l bates childs <labchilds@gmail.com>
2022-02-21 16:44:22 -0500

    change Post model to accept TEXT type, change tezt area size on dashboard and edit post

--- models/Post.js
+++ models/Post.js
@@ -46,7 +46,7 @@ Post.init(
             }
         },
         post_body: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false
         },
         user_id: {
